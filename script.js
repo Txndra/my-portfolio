13bbef18a0117a995622b79d4225d8ce3a516f38
Txndra <seandougan04@gmail.com>
2024-03-15 17:32:57 +0000

    feat: change styling and css

---
+++ script.js
@@ -0,0 +1,4 @@
+// add listener for the contact me button to go to a page with my contact details
+document.getElementById("contactMe").addEventListener("click", function () {
+  window.location.href = "contact.html";
+});
